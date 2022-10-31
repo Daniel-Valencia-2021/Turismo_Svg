04f378e8000f68add1d5c3475dd2bc44269320de
Daniel-Valencia-2021 <davalencia616@gmail.com>
2022-10-31 11:08:26 -0500

    No Cache

--- app/Http/Controllers/ZonaController.php
+++ app/Http/Controllers/ZonaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+
 class ZonaController extends Controller
 {
     public function index()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Zonas turisticas</title>
     <link rel="stylesheet" href="{{ asset('static/css/app.css') }}">
-
+    <title>Zonas turisticas</title>
 </head>
 
 <body>
