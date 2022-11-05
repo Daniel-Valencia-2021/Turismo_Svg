718567f922ec5bbcf112583a148955ebb21bdfff
Daniel-Valencia-2021 <davalencia616@gmail.com>
2022-11-05 03:14:48 -0500

    XD

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -17,6 +17,10 @@
 
     <title>@yield('title')</title>
 </head>
+<@php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+@endphp
 
 <body>
     <center> @yield('content')</center>
